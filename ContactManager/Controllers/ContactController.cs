b27f1cee5c14100184a9585cb5d718f2d5445354
Renata Barisic <rbarisic@etfos.hr>
2022-11-11 18:02:06 +0100

    Create a Read-Only Web API

---
+++ ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using ContactManager.Models;
+using ContactManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
